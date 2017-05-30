3b1d349a2dbf90f91f37d0857b81573eac27f6a3
abdul abdi <abdulrashid2@googlemail.com>
2017-05-30 21:18:29 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  profile = "sand-box"
+  profile = "profile"
   region  = "eu-west-1"
 }
 
 resource "aws_Account" "hmpo" {
 	
 	Arn = "1.2.3.4"
-	Id = "6778888"
+	Id = "12345"
 }
